5a936501932f8432a580cd72aa3f7ba9da76600a
Vrushali Murlidhar Patil <vrushalimpatil2001@gamil.com>
2023-04-20 17:01:17 +0530

    chat-room

---
+++ script.js
@@ -0,0 +1,18 @@
+let username = "";
+
+document.getElementById("join-btn").addEventListener("click", (event) =>{
+event.preventDefault();
+
+username = document.getElementById("username-input").value;
+
+// if input box is empty(username)  trim include the space
+if(username.trim() != ""){
+    document.querySelector(".form-username").style.display = "none";
+    document.querySelector(".chatroom-container").style.display = "block";
+}
+else{
+    alert("Username cannot be empty");
+}
+});
+
+
